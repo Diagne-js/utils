b8eb374b9650ef3aed2448d501b7f619a0fb64fa
Diagne.js <cheikhounadiagne358@gmail.com>
2025-04-07 14:03:37 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+export {newId} from './newId.js'
+export {getContent} from './getContent.js'
+export {getDynValues, replaceAll, escapeHtml, toTemplateLiteral} from './extra.js'
+export {objectTree} from './objectTree.js'
